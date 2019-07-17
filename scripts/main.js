5b9a282d9dc8b8ce9ced3ec6c4dd4d1c1516a88d
Won-Woo Chung <wwchung717@gmail.com>
2019-07-16 23:16:15 -0400

    Implemented draggable quick help

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,13 @@ let helpObj = document.getElementById('help');
 let helpIconObj = document.getElementById('help-icon')
 let helpCloseIconObj = document.getElementById('help-close-icon');
 let helpAppObj = document.getElementsByClassName('help-app')[0];
+let helpAppHeader = document.getElementById('help-app-header');
 
 function toggleHelp() {
   helpIconObj.classList.toggle('hidden');
   helpCloseIconObj.classList.toggle('hidden');
   helpAppObj.classList.toggle('help-app-visible');
+  setTimeout(resetDragHelp, 200);
 }
 
 // Help button
@@ -27,6 +29,40 @@ let helpItemObj = document.getElementById('help-item');
 helpItemObj.addEventListener('click', function() {
   toggleMenu();
   toggleHelp();
+});
+
+function startDragHelp(e) {
+  let distX = posX - e.clientX;
+  let distY = posY - e.clientY;
+  posX = e.clientX;
+  posY = e.clientY;
+  helpAppObj.style.top = `${helpAppObj.offsetTop - distY}px`;
+  helpAppObj.style.left = `${helpAppObj.offsetLeft - distX}px`;
+  helpAppObj.style.transition = 'none';
+}
+
+function stopDragHelp(e) {
+  document.onmouseup = null;
+  document.onmousemove = null;
+  helpAppObj.style.transition = '.2s';
+}
+
+function resetDragHelp() {
+  helpAppObj.style.top = null;
+  helpAppObj.style.left = null;
+  helpAppObj.style.bottom = '6.25rem';
+  helpAppObj.style.right = '1.25rem';
+}
+
+let posX;
+let posY;
+
+// Allow dragging
+helpAppHeader.addEventListener('mousedown', e => {
+  posX = e.clientX;
+  posY = e.clientY;
+  document.onmouseup = stopDragHelp;
+  document.onmousemove = startDragHelp;
 });
 
 // ----- SIDE NAV ----- //
